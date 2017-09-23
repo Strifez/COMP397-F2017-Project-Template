5e9e6e71d83289b0956faac88a8bbed030c99334
jhuan102 <300818592@cencol.ca>
2017-09-23 14:23:17 -0400

    first commit

---
+++ Scripts/core/game.js
@@ -0,0 +1,40 @@
+// IIFE - Immediately Invoked Function Expression
+(function () {
+    var stage;
+    var canvas;
+    var helloLabel;
+    var clickButton;
+    var assetManager;
+    var assetManifest = [
+        { id: "clickMeButton", src: "../../Assets/images/clickMeButton.png" }
+    ];
+    function Init() {
+        assetManager = new createjs.LoadQueue();
+        assetManager.installPlugin(createjs.Sound);
+        assetManager.on("complete", Start);
+        assetManager.loadManifest(assetManifest);
+    }
+    function Start() {
+        canvas = document.getElementById("canvas");
+        stage = new createjs.Stage(canvas);
+        stage.enableMouseOver(20);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", Update);
+        Main();
+    }
+    function Update() {
+        stage.update();
+    }
+    function Main() {
+        console.log("Game Started...");
+        helloLabel = new objects.Label("Hello World!", "40px", "Consolas", "#000000", 320, 240, true);
+        stage.addChild(helloLabel);
+        clickButton = new objects.Button(assetManager, "clickMeButton", 320, 340, true);
+        stage.addChild(clickButton);
+        clickButton.on("click", function () {
+            helloLabel.TextString = "GoodBye Cruel World";
+        });
+    }
+    window.onload = Init;
+})();
+//# sourceMappingURL=game.js.map
---
+++ Scripts/core/game.ts
@@ -0,0 +1,50 @@
+// IIFE - Immediately Invoked Function Expression
+(function(){
+  let stage:createjs.Stage;
+  let canvas:any;
+  let helloLabel:objects.Label;
+  let clickButton:objects.Button;
+  let assetManager:createjs.LoadQueue;
+
+  let assetManifest = [
+    {id: "clickMeButton", src:"../../Assets/images/clickMeButton.png"}
+  ];
+
+  function Init() {
+    assetManager = new createjs.LoadQueue();
+    assetManager.installPlugin(createjs.Sound);
+    assetManager.on("complete", Start);
+    assetManager.loadManifest(assetManifest);
+  }
+
+  function Start() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20);
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.on("tick", Update);
+    Main();
+  }
+
+  function Update() {
+    stage.update();
+  }
+
+  function Main() {
+    console.log("Game Started...");
+
+    helloLabel = new objects.Label("Hello World!", "40px", "Consolas", "#000000", 320, 240, true);
+    stage.addChild(helloLabel);
+
+    clickButton = new objects.Button(assetManager, "clickMeButton", 320, 340, true);
+    stage.addChild(clickButton);
+
+    clickButton.on("click", function() {
+      helloLabel.TextString = "GoodBye Cruel World";
+    });
+
+  }
+
+  window.onload = Init;
+
+})();
